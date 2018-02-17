4bb7a74ce11d17b6640007a567eaad4f7f59c743
logonrm <a>
2018-02-17 11:24:16 -0200

    gfhfghf

--- app/src/main/java/com/example/logonrm/demoorderedbroadcast/MainActivity.kt
+++ app/src/main/java/com/example/logonrm/demoorderedbroadcast/MainActivity.kt
@@ -1,18 +1,44 @@
 package com.example.logonrm.demoorderedbroadcast
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var receiver: BroadcastReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btEnviar.setOnClickListener{
+        btEnviar.setOnClickListener {
             sendBroadcast(Intent("MeuOrderedBroadcast"))
+
+            //
+            var filter = IntentFilter()
+            filter.addAction(Intent.ACTION_POWER_CONNECTED)
+            filter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+
+            receiver = object : BroadcastReceiver() {
+                override fun onReceive(context: Context?, intent: Intent?) {
+                    if (intent?.action.equals(Intent.ACTION_POWER_CONNECTED)) {
+                        tvUSBstatus.text = "Cabo conectado"
+                    } else {
+                        tvUSBstatus.text = "Cabo desconectado"
+                    }
+                }
+            }
+            registerReceiver(receiver, filter)
+
         }
+    }
+    override fun onDestroy() {
+        unregisterReceiver(receiver)
+        super.onDestroy()
     }
 }
